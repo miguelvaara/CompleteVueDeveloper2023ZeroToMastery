7b3f970de1242622874e8f9648dae1bef35501a7
miguelvaara <mika.vaara@helsinki.fi>
2023-05-10 22:40:02 +0300

    Muutamat muutokset

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
-// Vue.config.devtools = true
-Vue.createApp({}).mount('#app')
+const vm = Vue.createApp({
+  data() {
+    return {
+      firstName: "Suvi-Tuulia",
+      lastName: "Vaara",
+      url: 'http://www.google.com',
+    //   Huono ratkaisu, ei html:ää koodiin
+    raw_url: '<a href="https://www.google.fi" target="_blank">Kuukkeliin</a>',
+    //   Huono ratkaisu, ei html:ää koodiin
+      raw_html: '<input type="checkbox" id="jotain">',
+      age: 20
+    };
+  },
+  methods: {
+    fullName () {
+        return `Meitsin nomen est ${ this.firstName } ja sukunimi on ${ this.lastName.toUpperCase() }`
+    },
+    increment () {
+        this.age++
+    }
+  },
+}).mount("#app");
+
+// setTimeout(() => {
+//   vm.firstName = "Aatu";
+// }, 2000);
+
+// Vue.createApp({
+//     data() {
+//         return {
+//             firstName: 'Mika',
+//             lastName: 'Vaara'
+//         }
+//     }
+// }).mount('#app2')
